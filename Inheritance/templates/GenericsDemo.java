239d33c9000f5f90011aa08e87f2899e9b6034f6
Sophie Engle <sjengle@cs.usfca.edu>
2018-09-12 13:25:51 -0700

    Adding generics demo.

---
+++ Inheritance/templates/GenericsDemo.java
@@ -0,0 +1,34 @@
+public class GenericsDemo {
+
+	public static void main(String[] args) {
+
+		// TODO
+
+		/*
+		Iterator<A> iterator = elements.iterator();
+		Random r = new Random();
+
+		// if empty return null
+		if (!iterator.hasNext()) {
+			return null;
+		}
+
+		// get first item
+		A item = iterator.next();
+		int count = 1;
+
+		// loop through remaining items
+		while (iterator.hasNext()) {
+			A current = iterator.next();
+			count++;
+
+			// adjust probability replace item with current by count
+			if (r.nextInt(count + 1) < 1) {
+				item = current;
+			}
+		}
+
+		return item;
+		 */
+	}
+}
